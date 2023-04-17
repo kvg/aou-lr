d181ab3f7e546a697ffe0ee2b89a8da3aa5d1766
Kiran V Garimella <kiran@broadinstitute.org>
2023-04-17 00:22:39 -0400

    Improvements

--- scripts/build_docker_images.sh
+++ scripts/build_docker_images.sh
@@ -3,19 +3,14 @@
 set -euxo pipefail
 
 DOCKER_REPO="aou-lr"
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+LABEL=$1
 
 for DOCKER_FILE in $(find docker -name Dockerfile)
 do
     DIR_NAME=$(dirname $DOCKER_FILE)
     DOCKER_NAME=$(basename $DIR_NAME)
 
-    TAG="us-central1-docker.pkg.dev/broad-dsp-lrma/$DOCKER_REPO/$DOCKER_NAME:$BRANCH_NAME"
-
-    #docker build -t $TAG $DIR_NAME && \
-    #    gcloud config set account aou-lr-docker-builder@broad-dsp-lrma.iam.gserviceaccount.com && \
-    #    gcloud auth configure-docker -q us-central1-docker.pkg.dev && \
-    #    docker push $TAG
+    TAG="us-central1-docker.pkg.dev/broad-dsp-lrma/$DOCKER_REPO/$DOCKER_NAME:$LABEL"
 
     docker build -t $TAG $DIR_NAME && docker push $TAG
 done
