68c862b7e778450ec37ef3604be7671a8dc70087
Kiran V Garimella <kiran@broadinstitute.org>
2023-04-16 22:35:16 -0400

    Fixes to the Docker auto-build script

--- scripts/build_docker_images.sh
+++ scripts/build_docker_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DOCKER_REPO="aou-lr"
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 for DOCKER_FILE in $(find docker -name Dockerfile)
@@ -7,9 +8,13 @@ do
     DIR_NAME=$(dirname $DOCKER_FILE)
     DOCKER_NAME=$(basename $DIR_NAME)
 
-    TAG="us-central1-docker.pkg.dev/broad-dsp-lrma/$DOCKER_NAME:$BRANCH_NAME"
+    TAG="us-central1-docker.pkg.dev/broad-dsp-lrma/$DOCKER_REPO/$DOCKER_NAME:$BRANCH_NAME"
 
-    docker build -t $TAG $DIR_NAME
-    gcloud auth configure-docker -q
-    docker push $TAG
+    gcloud artifacts repositories create $DOCKER_REPO \
+        --repository-format=docker \
+        --location=us-central1 --description="Docker repository for AoU LR work"
+
+    docker build -t $TAG $DIR_NAME && \
+        gcloud auth configure-docker -q us-central1-docker.pkg.dev && \
+        docker push $TAG
 done
